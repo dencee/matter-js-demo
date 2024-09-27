94eb1a6440bf9cc933aee313cc1f5f59b0ec2c47
daniel commins <daniel.commins@techelevator.com>
2024-09-26 17:08:05 -0700

    add instructions

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ document.addEventListener("DOMContentLoaded", onDomLoaded);
 function onDomLoaded() {
   const canvas = document.querySelector("canvas");
 
+  //width and height of the page
+  let w = window.innerWidth
+  let h = window.innerHeight
+
   // module aliases
   let Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    Composites = Matter.Composites;
-  (Events = Matter.Events),
-    (Constraint = Matter.Constraint),
-    (MouseConstraint = Matter.MouseConstraint),
-    (Mouse = Matter.Mouse),
-    (World = Matter.World),
-    (Bodies = Matter.Bodies);
+    Composites = Matter.Composites,
+    Events = Matter.Events,
+    Constraint = Matter.Constraint,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World;
 
   // create an engine
   let engine = Engine.create();
@@ -25,47 +28,39 @@ function onDomLoaded() {
     canvas: canvas,
     engine: engine,
     options: {
+      width: w,
+      height: h,
+      showAngleIndicator: true,
+      background: "#000000",
       wireframes: false,
     },
   });
 
   // add bodies
-  let ground = Bodies.rectangle(395, 600, 815, 50, { isStatic: true }),
-    rockOptions = { density: 0.004 },
-    rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
-    anchor = { x: 170, y: 450 },
-    elastic = Constraint.create({
-      pointA: anchor,
-      bodyB: rock,
-      stiffness: 0.05,
-    });
+  let ground = Bodies.rectangle(w / 2, h, w, 150, { isStatic: true });
 
-  let pyramid = Composites.pyramid(500, 300, 9, 10, 0, 0, function (x, y) {
-    return Bodies.rectangle(x, y, 25, 40);
-  });
 
-  let ground2 = Bodies.rectangle(610, 250, 200, 20, { isStatic: true });
+  // TODO: Add boxes
+  // Bodies.rectangle(x, y, w, h);
+  //
+  let box = Bodies.rectangle(400, 200, 80, 80);
 
-  let pyramid2 = Composites.pyramid(550, 0, 5, 10, 0, 0, function (x, y) {
-    return Bodies.rectangle(x, y, 25, 40);
-  });
+  // TODO: Add 30 circles
+  // Bodies.circle(x, y, w, h, {options})
+  // Options: friction: 0.3
+  //          frictionAir: 0.1
+  //          restitution: 0.8
+  // https://brm.io/matter-js/docs/classes/Body.html
 
-  World.add(engine.world, [ground, pyramid, ground2, pyramid2, rock, elastic]);
+  // TODO: Create left and right walls (rectangles)
 
-  Events.on(engine, "afterUpdate", function () {
-    if (
-      mouseConstraint.mouse.button === -1 &&
-      (rock.position.x > 190 || rock.position.y < 430)
-    ) {
-      rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
-      World.add(engine.world, rock);
-      elastic.bodyB = rock;
-    }
-  });
+  // add bodies to our world
+  World.add(engine.world, [ground, box]);
+
 
   // add mouse control
-  let mouse = Mouse.create(render.canvas),
-    mouseConstraint = MouseConstraint.create(engine, {
+  let mouse = Mouse.create(render.canvas);
+  let mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
       constraint: {
         stiffness: 0.2,
@@ -75,16 +70,11 @@ function onDomLoaded() {
       },
     });
 
+  // Add the mouse to the world
   World.add(world, mouseConstraint);
 
   // keep the mouse in sync with rendering
   render.mouse = mouse;
-
-  // fit the render viewport to the scene
-  Render.lookAt(render, {
-    min: { x: 0, y: 0 },
-    max: { x: 800, y: 600 },
-  });
 
   // run the renderer
   Render.run(render);
